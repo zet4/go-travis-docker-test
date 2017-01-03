80b727a06d3a33d8b3a8e5a7e3a44bb0e023cba8
Aleksandr Tihomirov <hunter@zeta.pm>
2017-01-03 22:02:14 +0200

    this mby?

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    ginkgo -r --randomizeAllSpecs --randomizeSuites --failOnPending --cover -coverprofile=profile.out -covermode=atomic --trace --race --progress $d
+    go test -race -trace -ginkgo.progress -ginkgo.randomizeAllSpecs -test.coverprofile=profile.out -test.covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
